f163a6cf8e97f0868301e5ab479a3f9b0f57281e
tanitsu-reberu <tanitsu-reberu@users.noreply.github.com>
2026-06-29 08:29:47 +0000

    deploy: 0b7c4ee758db04016b139a2cc318ac56b69842ff

--- static/hub-config.js
+++ static/hub-config.js
@@ -1,3 +1,3 @@
-window.__HUB_API__ = '';
-window.__HUB_TOKEN__ = '';
+window.__HUB_API__ = 'https://office-hub-production.up.railway.app';
+window.__HUB_TOKEN__ = '18da40a2c02844ccb5641cbfba73bf52';
 window.__HUB_BASE__ = '/office-hub';
